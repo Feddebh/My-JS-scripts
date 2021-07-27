f1cfa6b621b832243a25c217c3b646438827d965
Feddebh <bluisfederico@gmail.com>
2021-07-27 01:25:20 -0300

    Practicing how to call a  property from an object  array- objarray.js

---
+++ objarray.js
@@ -0,0 +1,13 @@
+const web = {
+    nombre: "The odin project",
+    links: {
+        enlace: "www.theodinproject.com"
+    },
+    redesSociales: {
+        youtube:{
+            enlace: "www.youtube.com",
+            nombre: "canal de youtube"
+        }
+    }
+}
+console.log(web.redesSociales.youtube.enlace)
