21fb42f30fa9019b67d28a9f5c1540984faa15a8
Feddebh <bluisfederico@gmail.com>
2021-07-26 21:30:13 -0300

    agregue template.js

---
+++ template.js
@@ -0,0 +1,8 @@
+// template string
+
+const numero = (num1, num2) =>  (
+    `el numero es: ${num1 + num2}` // RETORNA la suma de los numeros que le pasamos a la constante
+    ) 
+const resultado = numero(15, 20);
+
+console.log(resultado)
