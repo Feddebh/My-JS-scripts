7ad6e11aeed5d409754098ef612a718b44e8606f
Feddebh <bluisfederico@gmail.com>
2021-07-30 02:53:04 -0300

    saving practice of callback functions 30/7/21

---
+++ foreachexcs.js
@@ -0,0 +1,23 @@
+//Callback function example:
+
+function function1() {
+    setTimeout(function(){
+        console.log("mensaje1");
+    }, 1000);
+}
+
+function function2() {
+    console.log("mensaje2");
+}
+
+function1();
+function2();
+
+function primero(){
+    console.log("primero");
+}
+
+function segundo(){
+    console.log("segundo");
+}
+
